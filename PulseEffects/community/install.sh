352f412d62ff69a99898dfa53656d597c5922343
Quint Guvernator <quint@guvernator.net>
2020-02-26 10:51:52 +0100

    PulseEffects: install script fixes

--- PulseEffects/community/install.sh
+++ PulseEffects/community/install.sh
@@ -6,15 +6,15 @@ function _startswith() { case "$1" in "$2"*) true;; *) false;; esac; }
 if [ "$1" = clean ]; then
     find "$XDG_CONFIG_HOME/PulseEffects" -type l | while read f; do
         dest="$(find "$f" -prune -printf "%l\n")"
-        printf '%s\n=>  %s\n\n' "$f" "$dest"
         if _startswith "$dest" "$XDG_CONFIG_HOME/PulseEffects/community"; then
-            echo rm "$f"
+            echo rm "$f" >&2
+            rm "$f"
         fi
     done
 
 else
     find "$XDG_CONFIG_HOME/PulseEffects/community" -type f -path '*.json' | while read f; do
-        ln -fs "$f" "$XDG_CONFIG_HOME/PulseEffects/"
-        echo "installed ${f##*/}"
+        ln -fs "$f" "$XDG_CONFIG_HOME/PulseEffects/input"
+        echo "${f##*/}" >&2
     done
 fi
