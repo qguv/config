7f1c9d996d70b421f337411c284534a6ac73d974
Quint Guvernator <quint@guvernator.net>
2020-04-08 21:20:17 +0200

    sway/slackfix: keep track of bad slack behavior to improve script

--- sway/slackfix.sh
+++ sway/slackfix.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
 set -e
 
-while true; do
-	cid="$(swaymsg --type subscribe '["window"]' | jq '
+while sleep 1; do
+	ev="$(swaymsg --type subscribe '["window"]' | jq --raw-output '
 		if (
-			.change == "new"
+			(.change == "new" or .change == "focus")
 			and .container.window_properties.class == "Slack"
-			and .container.geometry.width == 300
-			and .container.geometry.height == 56
 		) then
-			.container.id
+			(.container.id | tostring)
+			+ ":" + (.container.geometry.width | tostring)
+			+ "x" + (.container.geometry.height | tostring)
 		else
 			empty
 		end
 	')"
 
-	[ -z "$cid" ] && continue
+	[ -z "$ev" ] && continue
+
+	cid="${ev%:*}"
+	wh="${ev#*:}"
+	w="${wh%x*}"
+	h="${wh#*x}"
+
+	echo "$wh" >> ~/.config/sway/slack_windows.log
+
+	# 300 x 56 is the traditional "bad" size, but this was failing earlier and I don't know why. so we're gonna log more.
+	{ [ "$w" -ne 300 ] || [ "$h" -ne 56 ]; } && continue
+
 	swaymsg '[con_id='"$cid"']' kill
+	notify-send slackfix.sh "I killed a slack window ($ev) that looked like it was bugging out. If this is a problem, kill the shell running the 'slackfix.sh' script."
 done
