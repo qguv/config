73cee69dbf8fbf6c705c4d5290ed8dfc8cd96dc8
Quint Guvernator <quint@guvernator.net>
2022-05-27 16:55:56 +0200

    sway: sort screenshots into folders by year and month

--- sway/shot.sh
+++ sway/shot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export GRIM_DEFAULT_DIR="$HOME/Pictures/Screenshots"
+export GRIM_DEFAULT_DIR="$HOME/Pictures/Screenshots/$(date +%Y/%m)"
 usage='./shot.sh (region | monitor) [animate]'
 
 if killall -HUP wf-recorder 2>/dev/null; then
@@ -25,8 +25,9 @@ if [ "${2:-}" = animate ]; then
 
 	f="screencast_$(date +%11s)"
 	vid="/tmp/$f.mp4"
-	gif="$HOME/$f.gif"
+	gif="$GRIM_DEFAULT_DIR/$f.gif"
 
+	mkdir -p "$GRIM_DEFAULT_DIR"
 	wf-recorder "$flag" "$arg" -f "$vid"
 
 	ffmpeg -i "$vid" -vf 'fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse' -loop 0 "$gif"
@@ -36,9 +37,11 @@ if [ "${2:-}" = animate ]; then
 else
 	case "${1:-}" in
 	region)
+		mkdir -p "$GRIM_DEFAULT_DIR"
 		exec grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp)"
 		;;
 	monitor)
+		mkdir -p "$GRIM_DEFAULT_DIR"
 		exec grim -o "$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')"
 		;;
 	*)
