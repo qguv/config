81b65c270a8a3a6b50a4685f3c9d5aacb3ecf7b0
Quint Guvernator <quint@guvernator.net>
2022-11-28 11:39:45 +0100

    flipper backup.sh: fix logs

--- scripts/flipper/backup.sh
+++ scripts/flipper/backup.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 set -e
 
+log() {
+    printf '%s\n' "$*" >&2
+    if [ -n "$NOTIFY_SOCKET" ]; then
+        systemd-notify "--status=$*"
+    fi
+}
+
 SSH_KEY='/home/qguv/.config/ssh/id_ed25519'
 export GIT_SSH_COMMAND="ssh -o 'StrictHostKeyChecking no' -i '$SSH_KEY'"
 
 dump="$(mktemp -d /tmp/flipper-backup-dump-XXXXX)"
-systemd-notify --status="copying files from /media/flippersd to $dump"
+log "copying files from /media/flippersd to $dump"
 rsync --exclude '.Trash-*' -av /media/flippersd/ "$dump"
 
 clone="$(mktemp -d /tmp/flipper-backup-clone-XXXXX)"
-systemd-notify --status="downloading latest repository to $clone"
+log "downloading latest repository to $clone"
 git clone --no-checkout --branch sd --depth 1 'ssh://git@github.com/qguv/flipper_sd' "$clone"
 
-systemd-notify --status="setting up new repository in $dump"
+log "setting up new repository in $dump"
 mv "$clone/.git" "$dump/.git"
 rmdir "$clone"
 cd "$dump"
@@ -22,19 +29,19 @@ git config core.autocrlf true
 git config user.name 'Flipper SD'
 git config user.email 'flippersd@guvernator.net'
 
-systemd-notify --status="checking for changes"
+log "checking for changes"
 git add .
 git restore --staged .gitignore
 
 if git diff --cached --exit-code --quiet; then
     printf '%s\n' "no changes!"
 else
-    systemd-notify --status="committing changes"
+    log "committing changes"
     git commit -m 'update from SD'
 
-    systemd-notify --status="pushing changes"
+    log "pushing changes"
     git push -f origin sd
 fi
 
-systemd-notify --status="cleaning up"
+log "cleaning up"
 rm -rf "$dump"
